cea0e380d6cdc30e5eee7fc02e68ca7e93a44f82
Rohan Shrestha <151648002+raphael0002@users.noreply.github.com>
2024-10-19 19:09:58 +0545

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -75,60 +75,6 @@ export const skills = [
     }
 ];
 
-export const experiences = [
-    {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#accbe1",
-        date: "March 2020 - April 2021",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#fbc3bc",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#b7e4c7",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#a2d2ff",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-];
 
 export const socialLinks = [
     {
@@ -155,5 +101,12 @@ export const projects = [
         name: 'Recipe App',
         description: 'Meal Map is a comprehensive web application designed to allow users to discover, share, and manage recipes. Built using the MERN stack (MongoDB, Express.js, React.js, Node.js), the application supports user authentication, CRUD (Create, Read, Update, Delete) operations.',
         link: 'https://github.com/raphael0002/Meal-Map',
+    },
+    {
+        iconUrl: Chatify,
+        theme: 'btn-back-orange',
+        name: 'Chatify',
+        description: "Chatify is a modern chat application built with React and Firebase, offering real-time messaging, secure authentication, and cloud storage. Users can easily sign up, log in, and enjoy seamless conversations with friends, all while benefiting from Firebase's powerful backend services for scalability and data sync across devices.",
+        link: 'https://github.com/raphael0002/chatify',
     }
 ];
